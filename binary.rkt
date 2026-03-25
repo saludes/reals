3ce418d756c67811be1d4f50af5f3d1925fb3147
Jordi Saludes <jordi.saludes@upc.edu>
2026-03-25 09:37:25 +0100

    Revome 'streams'.

--- binary.rkt
+++ binary.rkt
@@ -1,7 +1,9 @@
 #lang racket
-;; (require "interval.rkt")
 (require racket/generator)
-(struct interval2 (mantissa exp))
+(require test-engine/racket-tests)
+(require "bisection.rkt")
+(provide dsqrt2 dsquare-root dcube-root)
+(provide stream-radix stream-exp stream-digits)
 
 (define (find-valid-interval p)
   (let loop ([B 1] [e 0])
@@ -9,90 +11,103 @@
         (values e (cons (- B) B))
         (loop (* 2 B) (add1 e)))))
 
-(define (bisect p)
-  (define-values (exp I) (find-valid-interval p))
-  (cons
-   exp
-   (generator ()
-             (let loop ([a (car I)] [b (cdr I)])
-               (define m (/ (+ a b) 2))
-               (cond
-                 [(xor (p a) (p m)) (yield #f) (loop a m)]
-                 [(xor (p m) (p b)) (yield #t) (loop m b)]
-                 [else (error "fail at [~a, ~a]" a b)])))))
-
-(define (bits->interval exp bits)
-  (define (mid a b) (/ (+ a b) 2))
-  (define I0
-    (let expand ([B 1] [n exp])
-      (if (= n 0) (cons (- B) B) (expand (* 2 B) (sub1 n)))))
-  (generator ()
-             (let loop ([a (car I0)] [b (cdr I0)])
-               (yield (cons a b))
-               (if (bits)
-                   (loop (mid a b) b)
-                   (loop a (mid a b))))))
-                 
-                 
-(define (imid I)
-   (/ (+ (car I) (cdr I)) 2))
-   
 
 
 ;; Decimal streams
 
 (struct stream (radix exp digits))
+(define (interval-decimate I)
+  (define dx (/ (interval-size I) 10))
+  (define (scale t) (+ (car I) (* t dx)))
+  (for/list ([i (in-range 10)])
+    (cons (scale i) (scale (add1 i)))))
 
+
+(define (decide p I) (xor (p (car I)) (p (cdr I))))
+
+(define (decimate-one p I)
+  (define result
+    (for/last ([J (interval-decimate I)] [i (in-range 10)])
+      #:final (decide p J)
+      (cons i J)))
+  (display (car result))
+  (cdr result))
+
+
+(define (decimate* p I0)
+  (forever-do (curry decimate-one p) I0))
+
+(define (expand p)
+  (define (expand-one I)
+    (cons (* 10 (car I)) (* 10 (cdr I))))
+  (let loop ([I (cons -1 1)])
+    (if (decide p I) I (loop (expand-one I)))))
+
+
+  
+  
 (define (decimate p)
-  (define (find10 I)
-    (define dx (/ (- (cdr I) (car I)) 10))
-    (define (scale t) (+ (car I) (* t dx)))
-    (define j
-      (for/last ([n (in-range 10)])
-      #:final (xor (p (scale n)) (p (scale (add1 n))))
-        n))
-    (values j (cons (scale j) (scale (add1 j)))))
-  (define-values (e I0)
-    (let loop ([A 5] [e 0])
-            (if (xor (p (- A)) (p A)) (values e (cons (- A) A)) (loop (* 10 A) (add1 e)))))
-  (stream
-   10
-   e
-   (generator ()
-             (let loop ([I I0])
-               (define-values (j Im) (find10 I))
-               (yield j)
-               (loop Im)))))
-
-
-(define (stream-approximate st N)
-  (define B (stream-radix st))
+  (define I-signed (expand p))
   (define I0
-         (let expand ([I (cons (- (/ B 2)) (/ B 2))] [n (stream-exp st)])
-           (if (= n 0) I (expand I (sub1 n)))))
-  (define digits (stream-digits st))
-  (let loop ([I I0] [k N])
-    (let* ([a (car I)]
-          [b (cdr I)]
-          [dx (/ (- b a) B)]
-          [scale (λ (t) (+ a (* t dx)))]
-          [d (digits)])
-      (if (= k 0)
-          I
-          (loop (cons (scale d) (scale (add1 d))) (sub1 k))))))
-      
-    
-    
+    (parameterize ([current-output-port (open-output-nowhere)]) ; put in a macro
+      (bisect-one p I-signed)))
+  (decimate* p I0))
 
+(define (digits-to g e)
+  (define (mirror ch)
+      (let ([ch0 (char->integer #\0)])
+        (case ch
+          [(#\.) #\.]
+          [else ((compose1 integer->char (λ (c) (+ 9 (* 2 ch0) (- c))) char->integer) ch)])))
+  (define mirror-string
+    (compose1 list->string (curry map mirror) string->list))
+  (define (steps x)
+    (inexact->exact (round (log x 10))))
+   (define digits
+     (with-output-to-string
+     (λ ()
+       (define I0  (g))
+       (display (if (zero? (car I0)) #\+ #\-))
+       ;(define N  (steps size))
+       (for ([I (in-producer g)]
+             [j (in-naturals 1)])
+         #:break (> j e)
+         (when (= (interval-size I) 1) (display #\.))))))
+  (case (string-ref digits 0)
+    [(#\-) (string-append "-" (mirror-string (substring digits 1)))]
+    [else  digits])) 
+  
+         ;(when (zero? (- N j)) (display #\.))))))
+  
+     
+  
 
 
 ;; Examples
 
-(define (sqr2 x) (and (> x 0) (> (* x x) 2)))
-(define bsqrt2 (bisect sqr2))
-(define dsqrt2 (decimate sqr2))
+(define (dsquare-root n)
+  (decimate (λ (x) (and (>= x 0) (> (* x x) n)))))
+(define (dcube-root n)
+  (decimate (λ (x) (> (* x x x) n))))
+(define dsqrt2 (dsquare-root 2))
 
 
-               
-
+;; Test
+(module+ test
+  (require rackunit)
+  (define-simple-check (check-small? I e)
+    (<= (interval-size I) e))
   
+  (define-simple-check (check-is-cut-for? I f)
+    (<= (* (f (car I)) (f (cdr I))) 0))
+  
+  (define (sq x) (* x x))
+
+  (define (approximate g steps)
+    (for/last ([I (in-producer g)]
+               [j (in-naturals)])
+        #:break (> j steps)
+      I))
+  (define Is2 (approximate dsqrt2 100))
+  (check-small? Is2 1e-80)
+  (check-is-cut-for? Is2 (λ (x) (- (sq x) 2))))
